be748c2bf1560114c268f98d3b20f69fe8018556
TwinkleToes6543 <verkon168@gmail.com>
2025-09-08 19:39:12 -0700

    Update Ossila.py

--- Ossila.py
+++ Ossila.py
@@ -1,14 +1,47 @@
-from xtralien import Device
 from decimal import Decimal
-import numpy as np
-import matplotlib.pyplot as plt
+import time
+import xtralien
 
 class Ossila():
     def measureVoltage(self):
-        port = "/dev/ttyACM0"
-        voltage = 0.7
+        port = "/dev/ttyACM0" # USB COM port number of the connected Source Measure Unit
+        channel = 'smu1'  # SMU channel to use
+        i_range = 1  # Current range to use, see manual for details
 
-        v = Decimal(str(voltage))  # use high precision
-        with Device(port) as smu:
-            result = smu.smu1.oneshot(v)[0]  # [voltage, current]
-            return float(result[0]), float(result[1])
+        # Parameters are defined using the Decimal class to avoid floating point errors
+        start_v = Decimal('0')  # Sweep start voltage in volts
+        end_v = Decimal('2.5')  # Sweep end voltage in volts
+        inc_v = Decimal('0.05')  # Sweep voltage increment in volts
+        power = []
+
+        # Connect to the Source Measure Unit using USB
+        with xtralien.Device(port) as SMU:
+            # Set the current range for SMU 1
+            SMU[channel].set.range(i_range, response=0)
+            time.sleep(0.05)
+            # Turn on SMU 1
+            SMU[channel].set.enabled(True, response=0)
+            time.sleep(0.05)
+
+            #Initialise the set voltage
+            set_v = start_v
+            # Loop through the voltages to measure
+            while set_v <= end_v:
+                # Set voltage, measure voltage and current
+                voltage, current = SMU[channel].oneshot(set_v)[0]
+
+                # Get power
+                power.append(current / 18 * voltage)
+
+                # Increment the set voltage
+                set_v += inc_v
+
+            # Get max power
+            mPower = max(power)
+
+            # Reset output voltage and turn off SMU 1
+            SMU[channel].set.voltage(0, response=0)
+            time.sleep(0.1)
+            SMU[channel].set.enabled(False, response=0)
+
+            return mPower
