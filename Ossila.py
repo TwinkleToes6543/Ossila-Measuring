cf8379542a7c2d2ad854af2704961596a0dd2581
TwinkleToes6543 <verkon168@gmail.com>
2025-09-16 19:49:26 -0700

    Update Ossila.py

--- Ossila.py
+++ Ossila.py
@@ -5,13 +5,13 @@ import xtralien
 class Ossila():
     def measureVoltage(self):
         port = "/dev/ttyACM0" # USB COM port number of the connected Source Measure Unit
-        channel = 'smu1'  # SMU channel to use
+        channel = 'smu2'  # SMU channel to use
         i_range = 1  # Current range to use, see manual for details
 
         # Parameters are defined using the Decimal class to avoid floating point errors
-        start_v = Decimal('0')  # Sweep start voltage in volts
-        end_v = Decimal('2.5')  # Sweep end voltage in volts
-        inc_v = Decimal('0.05')  # Sweep voltage increment in volts
+        start_v = Decimal('8.3')  # Sweep start voltage in volts
+        end_v = Decimal('-0.1')  # Sweep end voltage in volts
+        inc_v = Decimal('0.04')  # Sweep voltage increment in volts
         power = []
 
         # Connect to the Source Measure Unit using USB
@@ -26,12 +26,17 @@ class Ossila():
             #Initialise the set voltage
             set_v = start_v
             # Loop through the voltages to measure
-            while set_v <= end_v:
+            while set_v > end_v:
                 # Set voltage, measure voltage and current
-                voltage, current = SMU[channel].oneshot(set_v)[0]
+                # voltage, current = SMU[channel].oneshot(set_v)
+                result = SMU[channel].oneshot(set_v)
+                try:
+                    voltage, current = result[0]
+                except:
+                    break
 
                 # Get power
-                power.append(current / 18 * voltage)
+                power.append((current * voltage) / 10.23)
 
                 # Increment the set voltage
                 set_v += inc_v
