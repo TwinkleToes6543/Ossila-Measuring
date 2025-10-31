8114b454bc0b2aa4717a9615d6e6301b10eb4867
TwinkleToes6543 <verkon168@gmail.com>
2025-10-30 18:38:32 -0700

    Update getData.py

--- getData.py
+++ getData.py
@@ -1,22 +1,40 @@
 import time
+import datetime
 from spectrum_calib import SpecCal
 from Ossila import Ossila
-import datetime
 
 specCal = SpecCal()
 ossila = Ossila()
 
 while True:
-    devPower = ossila.measureVoltage()
-    sunPower = specCal.getWavelength()
-
+    # Measure JV and irradiance
+    devResults = ossila.measureVoltage()
+    sunPower_uWcm2 = specCal.getWavelength()  # returns µW/cm²
     currentTime = datetime.datetime.now()
 
-    with open("data.txt", "a") as f:
-        eff = float(devPower / sunPower * 100)
-        if eff < 0: eff *= -1
+    # Convert µW/cm² → W/cm², apply calibration factor
+    sunPower_mWcm2 = (sunPower_uWcm2 / 1_000)
 
-        f.write("\n" + str(eff) + "; Date: " + str(currentTime))
+    # Calculate efficiency (%)
+    eff = (devResults["mPower"] / sunPower_mWcm2) * 1000
+    eff = abs(eff)
 
+    ff = (eff) / (devResults["voc"] * devResults["jsc"])
+
+    print("voc:", devResults["voc"], "jsc:", devResults["jsc"], "ff:", ff, "eff:", eff)
+
+    if(ff > 1):
+        ff = 0.0
+        eff = 0.0
+
+    # Write data to file
+    with open("data/data9.txt", "a") as f:
+        f.write(
+            f"\nEfficiency: {eff:.2f}% | "
+            f"Jsc: {devResults["jsc"]:.3f} mA/cm² | "
+            f"Voc: {devResults["voc"]:.3f} V | "
+            f"Fill Factor: {ff:.3f} | "
+            f"Date: {currentTime}"
+        )
 
     time.sleep(10)
